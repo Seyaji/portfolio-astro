40c259e17d3d1c8cbcb4accca52141d57aff81cf
Alex <alexchelseaalex@gmail.com>
2024-02-17 20:20:30 +0000

    updates

---
+++ src/pages/api/skills.ts
@@ -0,0 +1,99 @@
+export const skills = [
+  {
+    title: "Primary Tech Stack",
+    id: "skill-one",
+    description: "My primary tech stack is focused around web apps buils with JavaScript based technologies",
+    skills: [
+      {
+        name: "JavaScript / TypeScript",
+        level: 95,
+        animation: 1,
+        description:
+          "Im confident in my TypeScript skills. I have been using it almost as long as I have known JavaScript. My recent commercial experience has been with React, Node and Canvass. Im confident developing APIs and front end applications.",
+      },
+      {
+        name: "React / Next.js",
+        level: 80,
+        animation: 2,
+        description:
+          "I have built a number of small apps with React (mainly Next.js right now). I have also worked on a large commercial project with React and Redux. I have a good understanding of the React ecosystem and can build functional components and hooks.",
+      },
+      {
+        name: "Node.js",
+        level: 90,
+        animation: 3,
+        description:
+          "Most of my projects begin with node (or Bun.sh because of the plug and play TypeScript). I have good commercial experience with Node and have built a number of APIs and services with it.",
+      },
+    ],
+  },
+  {
+    title: "Extra Tech",
+    id: "skill-two",
+    description: "I have a good understanding of a number of other technologies and tools",
+    skills: [
+      {
+        name: "BUN",
+        level: 95,
+        animation: 1,
+        description:
+          "I have been using BUN for a few months now and I love it. It's a great way to get a project up and running quickly with TypeScript and Node.",
+      },
+      {
+        name: "HTMX",
+        level: 80,
+        animation: 2,
+        description:
+          "I have been using HTMX for a few months now and I love it. It's a great way to get a project up and running quickly with TypeScript and Node.",
+      },
+      {
+        name: "ASTRO",
+        level: 90,
+        animation: 1,
+        description:
+          "I have been using ASTRO for a few months now and I love it. It's a great way to get a project up and running quickly with TypeScript and Node.",
+      },
+      {
+        name: "WAILS",
+        level: 90,
+        animation: 3,
+        description:
+          "I have been using WAILS for a few months now and I love it. It's a great way to get a crossplatform desktop app project up and running quickly with TypeScript and Go",
+      },
+    ],
+  },
+  {
+    title: "Learning Goals",
+    id: "skill-three",
+    description:
+      "I am always looking to learn new technologies and improve my skills. This is what Im working on improvoing right now.",
+    skills: [
+      {
+        name: "Go",
+        level: 60,
+        animation: 1,
+        description:
+          "I have been learning Go for a few months now and I love it. It's a great way to get a project up and running quickly with TypeScript and Node.",
+      },
+      {
+        name: "Rust",
+        level: 40,
+        animation: 2,
+        description:
+          "I have been learning Rust for a few months now and I love it. It's a great way to get a project up and running quickly with TypeScript and Node.",
+      },
+      {
+        name: "C",
+        level: 30,
+        description:
+          "I have been learning C for a few months now and I love it. It's a great way to get a project up and running quickly with TypeScript and Node.",
+      },
+      {
+        name: "Cyber Security",
+        level: 20,
+        description:
+          "This has recently become a big interest of mine. I have been learning about cyber security for a few months now and I love it.",
+      },
+    ],
+  },
+];
