054e1b5d19ac395b269fd3b031d746f8ea474b33
Alex <alexchelseaalex@gmail.com>
2024-02-18 10:46:20 +0000

    update cotent and UI

--- src/pages/api/skills.ts
+++ src/pages/api/skills.ts
@@ -2,7 +2,8 @@ export const skills = [
   {
     title: "Primary Tech Stack",
     id: "skill-one",
-    description: "My primary tech stack is focused around web apps buils with JavaScript based technologies",
+    description:
+      "My primary tech stack is focused around JavaScript based technologies. I have been using these for a number of years and have good commercial experience with them.",
     skills: [
       {
         name: "JavaScript / TypeScript",
@@ -16,49 +17,64 @@ export const skills = [
         level: 80,
         animation: 2,
         description:
-          "I have built a number of small apps with React (mainly Next.js right now). I have also worked on a large commercial project with React and Redux. I have a good understanding of the React ecosystem and can build functional components and hooks.",
+          "I have built a number of small apps with React (mainly Next.js recently). I have also worked in a corporate environment on a large commercial project with React, Node and Redux. I have a good understanding of the React ecosystem. I can build functional components, hooks and have also worked with larger legacy class component. I have built new complex class components to plugin to legacy systems also.",
       },
       {
         name: "Node.js",
         level: 90,
         animation: 3,
         description:
-          "Most of my projects begin with node (or Bun.sh because of the plug and play TypeScript). I have good commercial experience with Node and have built a number of APIs and services with it.",
+          "Most of my projects begin with node (or Bun.sh because of the plug and play TypeScript). I have good commercial experience with Node and have built a number of APIs and services with it. Node is widely used these days from small to large scale projects. Im confident in my node ability.",
       },
     ],
   },
   {
     title: "Extra Tech",
     id: "skill-two",
-    description: "I have a good understanding of a number of other technologies and tools",
+    description:
+      "I have a good understanding of a number of other technologies and tools. Im always pushing myself to learn new things. I keep up to date with the latest technologies and tools through learning and exploration. Here are some im using now.",
     skills: [
       {
-        name: "BUN",
-        level: 95,
-        animation: 1,
-        description:
-          "I have been using BUN for a few months now and I love it. It's a great way to get a project up and running quickly with TypeScript and Node.",
-      },
-      {
-        name: "HTMX",
-        level: 80,
-        animation: 2,
-        description:
-          "I have been using HTMX for a few months now and I love it. It's a great way to get a project up and running quickly with TypeScript and Node.",
-      },
-      {
-        name: "ASTRO",
+        name: "Bun.sh",
         level: 90,
         animation: 1,
         description:
-          "I have been using ASTRO for a few months now and I love it. It's a great way to get a project up and running quickly with TypeScript and Node.",
+          "Bun is such an excellent way to get started with a new concept. I really enjoy the type-safety of typescript and being able to just run typescript files without any setup is great. I really excited to see what happens with this project in the future. Good luck Jared and the rest o0f the Bun team!",
       },
       {
-        name: "WAILS",
+        name: "Vercel",
         level: 90,
         animation: 3,
         description:
-          "I have been using WAILS for a few months now and I love it. It's a great way to get a crossplatform desktop app project up and running quickly with TypeScript and Go",
+          "Vercel has been a game-changer for deploying my projects with ease and efficiency. Its seamless integration with a variety of frameworks it amazing. Its feature rich with useful metrics and a easy to use platform. I've become proficient in utilizing Vercel's features to optimize deployment workflows, ensuring fast, reliable, and secure delivery of web content.",
+      },
+      {
+        name: "HTMX",
+        level: 85,
+        animation: 2,
+        description:
+          "I have been using HTMX for a few months now and I love it. I really like the way it allows me to build dynamic web applications without having to write a lot of JavaScript. I have been using it with Go and Templ. I'm actually thinking about rewriting this site with HTMX and go as a side by side comparison. ",
+      },
+      {
+        name: "Linuxn - Debian",
+        level: 80,
+        animation: 2,
+        description:
+          "Linux is great. All power to the open source! I run a few VPS's with Debian that I use for my personal projects. I have good working ability and confidence running it through the command line. Hopefully onday soon I'll make a full switch to Linux from MacOS (or at least get a dedicated physical machine).",
+      },
+      {
+        name: "Astro",
+        level: 75,
+        animation: 1,
+        description:
+          "I have experimenting with Astro recently and its been really straight forward to pick up. A great way to build static sites with a bit of dynamic functionality. Its a good way to use the heavy frameworks (one or a number together) lightly. This was my first venture into Astro for a live project. The rest have been small experiments.",
+      },
+      {
+        name: "Wails",
+        level: 70,
+        animation: 3,
+        description:
+          "I have been building a few small apps with Wails recently and I love it. It's a great entrance into cross platform development and a comparitivley lightweight solution to some other choices out there. Pretty cool that you can directly call the Go code from the front end with their JS Bindings API.",
       },
     ],
   },
@@ -73,26 +89,26 @@ export const skills = [
         level: 60,
         animation: 1,
         description:
-          "I have been learning Go for a few months now and I love it. It's a great way to get a project up and running quickly with TypeScript and Node.",
+          "I have been learning Go on the side, trying to increase my hands on experience with it and I love it. I have done some Go & HTMX projects recently and they are  a great pair. My goal over the next few months is to really refine my knowledge here and build a few more projects with it. I have a few ideas for some small projects that I think would be great to build with Go.",
       },
       {
-        name: "Rust",
+        name: "Cyber Security",
         level: 40,
-        animation: 2,
         description:
-          "I have been learning Rust for a few months now and I love it. It's a great way to get a project up and running quickly with TypeScript and Node.",
+          "This has recently become a big interest of mine. I have been doing some small courses and learning materials on the side on and off. Its something I intent to build my skills up in over time.",
       },
       {
         name: "C",
         level: 30,
         description:
-          "I have been learning C for a few months now and I love it. It's a great way to get a project up and running quickly with TypeScript and Node.",
+          "Back when I began programming I started doing some C++ in unreal engine. Although that was a number of years ago. I Want to get a good foundation here. I think its such a core language in the tech world and while its not a necessity to learn it I think it would be a good thing to do. I have a couple courses in .Net and Unity that are on my list. (not vanilla C I know but I want more expreience with this language set altogether).",
       },
       {
-        name: "Cyber Security",
+        name: "Rust",
         level: 20,
+        animation: 2,
         description:
-          "This has recently become a big interest of mine. I have been learning about cyber security for a few months now and I love it.",
+          "This was going much better a while a go but it needs a revisit for sure. I dont like the fact that Im not very good at this and I like the concepts arpund Rust. More time ios needed here for sure",
       },
     ],
   },
